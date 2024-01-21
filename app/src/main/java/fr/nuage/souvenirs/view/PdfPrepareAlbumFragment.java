1ff42bb2f4b9ede2691480263c2dc67e762e39ca
zorgluf <zorgluf@gmail.com>
2024-01-21 21:55:19 +0100

    migrate to sdk33

--- app/src/main/java/fr/nuage/souvenirs/view/PdfPrepareAlbumFragment.java
+++ app/src/main/java/fr/nuage/souvenirs/view/PdfPrepareAlbumFragment.java
@@ -1,20 +1,28 @@
 package fr.nuage.souvenirs.view;
 
+import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.graphics.pdf.PdfDocument;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.ParcelFileDescriptor;
 import android.print.PrintAttributes;
 import android.print.pdf.PrintedPdfDocument;
+import android.provider.DocumentsContract;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityOptionsCompat;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 import androidx.core.content.FileProvider;
@@ -26,6 +34,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.concurrent.Executors;
@@ -45,6 +55,7 @@ public class PdfPrepareAlbumFragment extends Fragment {
     private int resolution;
     private PrintedPdfDocument document;
     private AlertDialog waitDialog;
+    private int CREATE_PDF_FILE = 1;
 
 
     @Override
@@ -79,23 +90,13 @@ public class PdfPrepareAlbumFragment extends Fragment {
         params.width = pageSize;
         mainLayout.setLayoutParams(params);
 
-        //start page generation
-        albumVM.getLdPages().observe(getViewLifecycleOwner(), pageViewModels -> {
-            if (pageViewModels != null) {
-                ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
-                executor.schedule(() -> {
-                    exportToPdf(mainLayout,pageViewModels);
-                    waitDialog.dismiss();
-                    Navigation.findNavController(container).popBackStack();
-                },2, TimeUnit.SECONDS);
-            }
-        });
-
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle(R.string.pdf_export_wait)
                 .setCancelable(false);
         waitDialog = builder.create();
         waitDialog.show();
+
+        createFile();
 
         return v;
     }
@@ -111,9 +112,40 @@ public class PdfPrepareAlbumFragment extends Fragment {
         document = new PrintedPdfDocument(getContext(),printAttributes);
     }
 
-    private void exportToPdf(FrameLayout mainLayout, ArrayList<PageViewModel> pageViewModels) {
+    private void createFile() {
+        Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("application/pdf");
+        intent.putExtra(Intent.EXTRA_TITLE, albumVM.getName().getValue()+".pdf");
+        try {
+            intent.putExtra(DocumentsContract.EXTRA_INITIAL_URI, new URI(new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath(),albumVM.getName().getValue()+".pdf").getPath()));
+        } catch (URISyntaxException e) {
+        }
+        startActivityForResult(intent, CREATE_PDF_FILE);
+    }
 
-        String pdfPath = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath(),albumVM.getName().getValue()+".pdf").getPath();
+    @Override
+    public void onActivityResult(int requestCode, int resultCode,
+                                 Intent resultData) {
+        if ((requestCode == CREATE_PDF_FILE) && (resultCode == Activity.RESULT_OK)) {
+            Uri uri;
+            if (resultData != null) {
+                uri = resultData.getData();
+                ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+                executor.schedule(() -> {
+                    exportToPdf(uri);
+                    waitDialog.dismiss();
+                    Navigation.findNavController(getView()).popBackStack();
+                },2, TimeUnit.SECONDS);
+            }
+        }
+    }
+
+
+    private void exportToPdf(Uri outUri) {
+
+        ArrayList<PageViewModel> pageViewModels = albumVM.getLdPages().getValue();
+        FrameLayout mainLayout = getView().findViewById(R.id.pdf_main_layout);
 
         //render page by page
         for (PageViewModel pageViewModel : pageViewModels) {
@@ -133,22 +165,29 @@ public class PdfPrepareAlbumFragment extends Fragment {
         }
         // write the document to file
         try {
-            FileOutputStream out = new FileOutputStream(new File(pdfPath));
-            document.writeTo(out);
+            ParcelFileDescriptor pfd = getActivity().getContentResolver().
+                    openFileDescriptor(outUri, "w");
+            FileOutputStream fileOutputStream =
+                    new FileOutputStream(pfd.getFileDescriptor());
+            document.writeTo(fileOutputStream);
+            // Let the document provider know you're done by closing the stream.
+            fileOutputStream.close();
+            pfd.close();
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            Log.e(this.getClass().getName(),"File not found", e);
         } catch (IOException e) {
-            e.printStackTrace();
+            Log.e(this.getClass().getName(),"IO error", e);
         }
+
         // close the document
         document.close();
-        Log.i(getClass().getName(),"Export to pdf finished to "+pdfPath);
+        Log.i(getClass().getName(),"Export to pdf finished to " + outUri.getPath());
 
         //create notification
         Intent shareIntent = new Intent();
         shareIntent.setAction(Intent.ACTION_VIEW);
-        Uri pdfUri = FileProvider.getUriForFile(getContext(),getContext().getApplicationContext().getPackageName() + ".provider", new File(pdfPath));
-        shareIntent.setDataAndType(pdfUri, "application/pdf");
+        //Uri pdfUri = FileProvider.getUriForFile(getContext(),getContext().getApplicationContext().getPackageName() + ".provider", new File(pdfPath));
+        shareIntent.setDataAndType(outUri, "application/pdf");
         shareIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_GRANT_READ_URI_PERMISSION);
         PendingIntent pendingIntent = PendingIntent.getActivity(getContext(), 0, shareIntent, PendingIntent.FLAG_IMMUTABLE);
         NotificationCompat.Builder nBuilder = new NotificationCompat.Builder(getContext(), AlbumListActivity.CHANNEL_ID);
